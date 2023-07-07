e6fe85428baa268da64602f518a366fa8dafe7e5
threejay20 <118399217+threejay20@users.noreply.github.com>
2023-07-07 19:44:32 -0400

    sidebar

--- src/scenes/global/SideBar.jsx
+++ src/scenes/global/SideBar.jsx
@@ -1,5 +1,79 @@
+import { useState } from "react";
+import { ProSidebar, Menu, MenuItem } from "react-pro-sidebar";
+import 'react-pro-sidebar/dist/css/stles.css';
+import { Box, IconButton, Typography, useTheme } from '@mui/material';
+import { Link } from "react-router-dom";
+import { tokens } from "../../theme";
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import PeopleOutlinedIcon from "@mui/icons-material/PeopleOutlined";
+import ContactsOutlinedIcon from "@mui/icons-material/ContactsOutlined";
+import ReceiptOutlinedIcon from "@mui/icons-material/ReceiptOutlined";
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+import CalendarOutlinedIcon from "@mui/icons-material/CalendarOutlined";
+import HelpOutlinedIcon from "@mui/icons-material/HelpOutlined";
+import BarChartOutlinedIcon from "@mui/icons-material/BarChartOutlined";
+import PieChartOutlineOutlinedIcon from "@mui/icons-material/PieChartOutlineOutlined";
+import TimelineOutlinedIcon from "@mui/icons-material/TimelineOutlined";
+import MenuOutlinedIcon from "@mui/icons-material/MenuOutlined";
+import MapOutlinedIcon from "@mui/icons-material/MapOutlined";
+import { MenuBookOutlined } from "@mui/icons-material";
+
+
+
 const SideBar = () => {
-    return  <div> SideBar </div>
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode);
+    const [isCollapsed, setIsCollapsed] = useState(false);
+    const [selected, setSelected] = useState('Dashboard')
+    
+    return  (
+        <Box
+            sx={{
+                "& .pro-sidebar-inner": {
+                    background: `${colors.primary[400]} !important`,
+                },
+                "& .pro-icon-wrapper": {
+                    backgroundColor: "transparent !important",
+                },
+                "& .pro-inner-item" : {
+                    padding: "5px 35px 5px 20px !important",
+                },
+                "& .pro-inner-item-hover": {
+                    color: "#868dffb !important",
+                }, 
+                "& .pro-menu-item.active" : {
+                    color: "#6870fa !important",
+                }
+            }}
+        >
+            <ProSidebar collapsed={isCollapsed}>
+                <Menu iconShape="square">
+                    {/* {LOGO AND MENU ICON} */}
+                    <MenuItem
+                        onClick={() => setIsCollapsed(!isCollapsed)}
+                        icon={isCollapsed ? <MenuOutlinedIcon/> : undefined}
+                        style={{
+                            margin: "10px 0 20px 0",
+                            color: colors.grey[100],
+                        }}
+                    >
+                        {!isCollapsed && (
+                            <Box>
+                                <Typography>
+                                  Head Of The Table  
+                                </Typography>
+                                <IconButton>
+                                    <MenuOutlinedIcon />
+                                </IconButton>
+                            </Box>
+                        ) }
+                        
+                        </MenuItem>
+                </Menu>
+            </ProSidebar>
+
+        </Box>
+    );
 }
 
 export default SideBar;
