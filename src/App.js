2d9b882f48d1be0f2f2fa5b3aab30e2e16b0edaf
threejay20 <118399217+threejay20@users.noreply.github.com>
2023-07-11 16:50:59 -0400

    contacts sections

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import { ColorModeContext, useMode } from './theme';
 import { CssBaseline, ThemeProvider } from '@mui/material';
-import Topbar from './scenes/global/Topbar';
+import Topbar from './scenes/global/TopBar';
 import { Route, Routes } from 'react-router-dom';
 import Dashboard from "./scenes/dashboard";
 import Sidebar from './scenes/global/SideBar';
 import { useState } from 'react';
 import Team from "./scenes/team";
 // import Invoices from "./scenes/invoices";
-// import Contacts from "./scenes/contacts";
+import Contacts from "./scenes/contacts";
 // import Bar from "./scenes/bar";
 // import Form from "./scenes/form";
 // import Line from "./scenes/line";
@@ -32,7 +32,7 @@ function App() {
           <Routes>
             <Route path="/" element={<Dashboard />} />
             <Route path="/team" element={<Team />} />
-            {/* <Route path="/contacts" element={<Contacts />} /> */}
+            <Route path="/contacts" element={<Contacts />} />
             {/* <Route path="/invoices" element={<Invoices />} /> */}
             {/* <Route path="/form" element={<Form />} /> */}
             {/* <Route path="/bar" element={<Bar />} /> */}
