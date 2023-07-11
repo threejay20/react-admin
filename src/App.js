dfd291690b7cae38fe667d4fda679461b662bcf4
threejay20 <118399217+threejay20@users.noreply.github.com>
2023-07-11 17:17:31 -0400

    invoices

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Dashboard from "./scenes/dashboard";
 import Sidebar from './scenes/global/SideBar';
 import { useState } from 'react';
 import Team from "./scenes/team";
-// import Invoices from "./scenes/invoices";
+import Invoices from "./scenes/invoices";
 import Contacts from "./scenes/contacts";
 // import Bar from "./scenes/bar";
 // import Form from "./scenes/form";
@@ -33,7 +33,7 @@ function App() {
             <Route path="/" element={<Dashboard />} />
             <Route path="/team" element={<Team />} />
             <Route path="/contacts" element={<Contacts />} />
-            {/* <Route path="/invoices" element={<Invoices />} /> */}
+            <Route path="/invoices" element={<Invoices />} />
             {/* <Route path="/form" element={<Form />} /> */}
             {/* <Route path="/bar" element={<Bar />} /> */}
             {/* <Route path="/pie" element={<Pie />} /> */}
